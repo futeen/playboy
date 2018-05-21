51623239edc0e39855e2b1731e4370f447a1fbbd
Futeen <futeen14@gmail.com>
2018-05-21 19:07:59 +0800

    super().__init__()

---
+++ basic/thread/super.py
@@ -0,0 +1,18 @@
+class Base(object):
+    def __init__(self):
+        print("Base created")
+
+
+class ChildA(Base):
+    def __init__(self):
+        Base.__init__(self)
+
+
+class ChildB(Base):
+    def __init__(self):
+        super().__init__()
+
+
+ChildA()
+ChildB()
+
