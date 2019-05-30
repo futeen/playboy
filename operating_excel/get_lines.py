76300319c37632fa31cb44c80789f3432cd36791
futeen <nigulasikaiqi@yeah.net>
2019-05-30 13:35:43 -0400

    finish 2nd version

--- operating_excel/get_lines.py
+++ operating_excel/get_lines.py
@@ -1,3 +1,8 @@
+
+rt pandas as pd
+from codes.cdb import integrete_data
+
+
 def get_lines():
     id_list_nine = []
     id_list_result = []
@@ -6,8 +11,23 @@ def get_lines():
     id = df_nine["*证照号码"].values.tolist()
     print(id)
     for x in id:
-        id_nine = df_nine.loc[df_nine["*证照号码"] == x]
+        id_nine = df_nine.loc[df_nine["*证照号码"] == x].values.tolist()[0][5:]
         id_list_nine.append(id_nine)
-        id_result = df_result.loc[df_result["*证照号码"] == "{0}".format(x)]
+        id_result = df_result.loc[df_result["*证照号码"] == "{0}".format(x)].values.tolist()[0][5:]
         id_list_result.append(id_result)
+    a = []
+    b = []
+    for x in id_list_nine:
+        x = x[:2] + x[-3:-2]
+        a.append(x)
+    for x in id_list_result:
+        x = x[:2] + x[-3:-2]
+        b.append(x)
+    print(a)
     print(b)
+
+
+
+if __name__ == "__main__":
+    get_lines()
+
